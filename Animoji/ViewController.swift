984294c25732c0bf90c550b2f93717692a919a02
yc3359 <yc3359@users.noreply.github.com>
2020-11-05 15:44:10 -0500

    git push

--- Animoji/ViewController.swift
+++ Animoji/ViewController.swift
@@ -7,18 +7,68 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet var arView: ARView!
+    var duckAnchor: DuckExperience.Animoji!
+    var allowsTalking = true
+    var upperMouth: Entity!
+    var bottomMouth: Entity!
+    var upperMouthModel: ModelEntity!
+    var bottomMouthModel: ModelEntity!
+    var lEye: Entity!
+    var rEye: Entity!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        let config = ARFaceTrackingConfiguration()
+        arView.session.run(config)
+        arView.session.delegate = self
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        // Load the "Animoji" scene from the "Experience" Reality File
+        duckAnchor = try! DuckExperience.loadAnimoji()
+        
+        // Add the duck anchor to the scene
+        arView.scene.anchors.append(duckAnchor)
+        
+        upperMouth = duckAnchor.findEntity(named: "upmouth")
+        bottomMouth = duckAnchor.findEntity(named: "botmouth")
+        
+        upperMouthModel = upperMouth.children.first as? ModelEntity
+        bottomMouthModel = bottomMouth.children.first as? ModelEntity
+        
+        lEye = duckAnchor.findEntity(named: "lefteye")
+        rEye = duckAnchor.findEntity(named: "righteye")
+        
+        duckAnchor.actions.finishedTalking.onAction = { _ in
+            self.allowsTalking = true
+        }
+        
+    }
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        var faceAnchor: ARFaceAnchor?
+        
+        for i in anchors {
+            if let anchors = i as? ARFaceAnchor {
+                faceAnchor = anchors
+            }
+        }
+        guard let blendShapes = faceAnchor?.blendShapes,
+              let jawV = blendShapes[.jawOpen]?.floatValue,
+              let lEyeV = blendShapes[.eyeBlinkLeft]?.floatValue,
+              let rEyeV = blendShapes[.eyeBlinkRight]?.floatValue else { return }
+        
+        
+        bottomMouth.position.z = 0.015 + jawV * 0.02
+        upperMouth.position.z = 0 - jawV * 0.005
+        if (jawV >= 0.5) && allowsTalking {
+            allowsTalking = false
+            duckAnchor.notifications.talk.post()
+        }
+        lEye.scale.z = 1.25 - lEyeV
+        rEye.scale.z = 1.25 - rEyeV
     }
 }
